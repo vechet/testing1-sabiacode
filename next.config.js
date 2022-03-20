9ca42716482b1553e625387e0d4cdf74bad526e6
vechet <vechet240@gmail.com>
2022-03-20 14:41:08 +0700

    configure styled-components with next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
     domains: ["i.scdn.co", "images.unsplash.com", "t.scdn.co"],
     formats: ["image/webp"],
   },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 };
